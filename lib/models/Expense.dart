6fa8bb2b9e61d74098f52714950fe7584aae3846
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-02-10 14:19:39 +0300

    Create Expense model

---
+++ lib/models/Expense.dart
@@ -0,0 +1,9 @@
+class Expense {
+  String title;
+  String description;
+  String amount;
+  String imageUrl;
+  String expenseId;
+
+  Expense({this.title, this.description, this.amount, this.imageUrl,this.expenseId});
+}
